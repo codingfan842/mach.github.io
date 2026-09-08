b0ba70837794ecef3d7645a5ad14a55445cc4b7f
dorianstuffs <dorianmoes@proton.me>
2026-09-08 04:54:39 -0500

    Updated README and .eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
         eleventyConfig.addPassthroughCopy("src");
+        eleventyConfig.addPassthroughCopy("CNAME");
+
     return {
         markdownTemplateEngine: "njk"
     };
